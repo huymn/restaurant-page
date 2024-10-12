51b114d050d5459c785e6cad89fe0b3618a15b3d
Huy Nguyen <huynguyen560@gmail.com>
2024-10-11 18:35:13 -0700

    feat: add webpack config and basic html and javascript file
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
