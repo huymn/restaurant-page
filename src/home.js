4ea891b1f62654dff8a19b1f1ab64f4ecedaa8f0
Huy Nguyen <huynguyen560@gmail.com>
2024-10-13 13:37:10 -0700

    feat: add css for home page and adjust some elements in home page js file
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- src/home.js
+++ src/home.js
@@ -1,19 +1,25 @@
 import restaurantBackground from "./assets/restaurant-background.jpg";
 
 function createHomePage(contentDiv) {
-  const restaurantBackgroundImg = document.createElement("img");
   const mainContainerDiv = document.createElement("div");
-  const mainHeader = document.createElement("h1");
-  const mainMessage = document.createElement("p");
+  mainContainerDiv.classList.add("home-container");
 
-  restaurantBackgroundImg.src = restaurantBackground;
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add("home-message-container");
+
+  const mainHeader = document.createElement("h1");
+  mainHeader.classList.add("home-header");
+
+  const mainMessage = document.createElement("p");
+  mainMessage.classList.add("home-message");
 
   mainHeader.innerText = "Welcome to Huy's restaurant!";
   mainMessage.innerText =
     "This restaurant includes Huy's favorite food only. Check out the menu";
 
-  mainContainerDiv.append(mainHeader, mainMessage);
-  contentDiv.append(restaurantBackgroundImg, mainContainerDiv);
+  messageContainer.append(mainHeader, mainMessage);
+  mainContainerDiv.append(messageContainer);
+  contentDiv.append(mainContainerDiv);
 }
 
 export default createHomePage;
